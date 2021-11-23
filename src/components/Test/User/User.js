a00317d236f18b4d619b78c3c5c270a8697cd5b7
Alain Gourvès <alain.gourves@gmail.com>
2021-11-23 12:25:42 +0100

    Ajout champs User

--- src/components/Test/User/User.js
+++ src/components/Test/User/User.js
@@ -6,32 +6,49 @@ import { useForm } from 'react-hook-form';
 
 function Home() {
     const [redirect, setRedirect] = useState(null);
-
-    const { reset, register, handleSubmit, watch } = useForm({
+    
+    const { reset, register, handleSubmit, watch, setValue } = useForm({
         defaultValues: {
+            'initials': '',
+            'uniqueId': '',
+            'birthDate': '',
             'age': 0,
             'gender': 'M',
             'etablissement': ''
         }
     })
+    
+    const watchBirthDate = watch('birthDate');
+    useEffect(() => {
+        const d = watchBirthDate.split('-');
+        // Calcul de l'âge
+        const d1 = new Date(); //aujourd'hui
+        const d2 = new Date(d[0], d[1]-1, d[2]); // Y,M,D (month counter is zero based)
+        const age = d1.getFullYear() - d2.getFullYear();
+        if(!Number.isNaN(age) && age < 100) {
+            setValue('age', age)
+        }
+    }, [watchBirthDate, setValue]);
 
     const watchGender = watch('gender')
-
     useEffect(() => {
         let bgCol;
         if (watchGender === 'M') {
             bgCol = 'blue';
-        }else if(watchGender === 'F') {
+        } else if (watchGender === 'F') {
             bgCol = 'pink'
-        }else{
+        } else {
             bgCol = 'gray'
         }
         document.documentElement.style.setProperty('--bg-carte', `var(--bg-${bgCol})`);
     }, [watchGender])
-    
-    useEffect(()=>{
-        if(localStorage.getItem('user') && localStorage.getItem('user') !== null) {
+
+    useEffect(() => {
+        if (localStorage.getItem('user') && localStorage.getItem('user') !== null) {
             const defaultValues = {
+                'uniqueId': JSON.parse(localStorage.getItem('user')).uniqueId,
+                'initials': JSON.parse(localStorage.getItem('user')).uniqueId.substring(0, 2),
+                'birthDate': JSON.parse(localStorage.getItem('user')).birthDate,
                 'age': JSON.parse(localStorage.getItem('user')).age,
                 'gender': JSON.parse(localStorage.getItem('user')).gender,
                 'etablissement': JSON.parse(localStorage.getItem('user')).etablissement
@@ -41,7 +58,15 @@ function Home() {
     }, [reset])
 
     const onSubmit = (vals) => {
+        // Création de `uniqueId` : initiales + (année x mois x jour) en hexadécimal
+        const d = vals.birthDate.split('-').reduce((prev, next) => {
+            return parseInt(prev) * parseInt(next)
+        }).toString(16);
+        const uniqueId = vals.initials.toUpperCase() + d.toUpperCase();
+        console.log(uniqueId)
         const obj = {
+            'uniqueId': uniqueId,
+            'birthDate': vals.birthDate,
             'age': vals.age,
             'gender': vals.gender,
             'etablissement': vals.etablissement
@@ -59,6 +84,24 @@ function Home() {
             <p className="text-center">Commence par donner quelques informations sur toi.</p>
             <form onSubmit={handleSubmit(onSubmit)}>
                 <div className="my-4">
+                    <div className="row align-items-center mb-3">
+                        <label htmlFor="age" className="form-label col-md-4 text-end">Tes initiales:</label>
+                        <div className="col-md-8">
+                            <input
+                                {...register("initials")}
+                                type="text"
+                                className="form-control text-uppercase" />
+                        </div>
+                    </div>
+                    <div className="row align-items-center mb-3">
+                        <label htmlFor="age" className="form-label col-md-4 text-end">Ta date de naissance:</label>
+                        <div className="col-md-8">
+                            <input
+                                {...register("birthDate")}
+                                type="date"
+                                className="form-control" />
+                        </div>
+                    </div>
                     <div className="row align-items-center mb-3">
                         <label htmlFor="age" className="form-label col-md-4 text-end">Ton âge:</label>
                         <div className="col-md-8">
