5ea5491cc8979b9219d97d12c1741a3c1157f366
Alain Gourvès <alain.gourves@gmail.com>
2021-11-29 17:34:42 +0100

    Login avec Hook Form et validation

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,13 +1,14 @@
 import './login.scss';
-import { useRef, useState } from 'react';
+import { useState } from 'react';
 import { useHistory, Link } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
 import { useAuth } from '../../contexts/Auth';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
+import AlertMesg from '../AlertMesg/AlertMesg';
 import { Eye, EyeOff } from 'react-feather';
 
 const Login = function () {
-    const emailRef = useRef();
-    const passwordRef = useRef();
-
     const [error, setError] = useState(null);
 
     const [eyeIcon, setEyeIcon] = useState(true); // true -> Eye, false -> EyeOff
@@ -17,49 +18,79 @@ const Login = function () {
 
     const history = useHistory();
 
-    const handleSubmit = async (ev) => {
-        ev.preventDefault();
+    const schema = yup.object().shape({
+        email: yup.string().email("Vérifier l'adresse mail, elle ne semble pas correcte.").required("Merci de renseigner votre adresse mail."),
+        password: yup.string().required("Merci de saisir votre mot de passe."),
+    });
+
+    const { register, handleSubmit, formState: { errors } } = useForm({
+        defaultValues: {
+            'email': '',
+            'password': '',
+        },
+        resolver: yupResolver(schema),
+    });
+
+    const onSubmit = async (values) => {
         // Get input values
-        const email = emailRef.current.value;
-        const password = passwordRef.current.value;
+        const email = values.email;
+        const password = values.password;
         // Calls signIn function from the context
         const { error } = await signIn({ email, password });
 
         if (error) {
-            return setError(error);
+            console.log("Erreur signIn(): ", error);
+            setError("Les informations fournies ne sont pas reconnues, vérifiez votre saisie.");
+        } else {
+            history.push('/');
         }
-        history.push('/')
+    }
+    const onError = (errors) => {
+        console.log("erreur onSubmit(): ", errors);
     }
 
     const showPassword = () => {
         setEyeIcon(!eyeIcon);
-        passwordRef.current.type = (eyeIcon) ? 'text' : 'password';
     }
 
     return (
-        <form onSubmit={handleSubmit}>
-            {error &&
-                <div className="alert alert-warning mb-3">{error && JSON.stringify(error)}</div>
-            }
+        <form onSubmit={handleSubmit(onSubmit, onError)}>
 
+            {error &&
+                <AlertMesg message={error} />
+            }
             <div className="col mb-3">
-                <label className="form-label" htmlFor="input-email">Email</label>
-                <input className="form-control" name="input-email" type="email" ref={emailRef} />
+                <label className="form-label" htmlFor="email">Email</label>
+                <input
+                    {...register("email")}
+                    className={`form-control ${error ? 'is-invalid':''}`}
+                    type="email"
+                />
+                {errors.email &&
+                    <AlertMesg message={errors.email?.message} />
+                }
             </div>
 
             <div className="col mb-3">
-                <label className="form-label" htmlFor="input-password">Mot de passe</label>
+                <label className="form-label" htmlFor="password">Mot de passe</label>
                 <div className="input-group">
-                    <input className="form-control" name="input-password" type="password" ref={passwordRef} />
+                    <input
+                        {...register("password")}
+                        className={`form-control ${error ? 'is-invalid':''}`}
+                        type={eyeIcon ? "password" : "text"}
+                    />
                     <span className="eye input-group-text" onClick={showPassword}>
                         {eyeIcon ? <Eye size={18} /> : <EyeOff size={18} />}
                     </span>
                 </div>
+                {errors.password &&
+                    <AlertMesg message={errors.password?.message} />
+                }
             </div>
 
             <div className="d-flex flex-column justify-content-center align-items-center">
-                <button className="btn btn-primary mb-2" type="submit">Login</button>
-                <p>Pas de compte ? <Link to="/inscription">Inscrivez-vous</Link></p>
+                <button className="btn btn-primary mb-2" type="submit">Connexion</button>
+                <p>Pas encore de compte ? <Link to="/inscription">Inscrivez-vous</Link></p>
             </div>
 
         </form>
