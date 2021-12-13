c7847e9cdb9f04d9b7c5e5e93e3e2534cd802062
Alain Gourvès <alain.gourves@gmail.com>
2021-12-13 18:07:50 +0100

    /User form validation

--- src/components/Test/User/User.js
+++ src/components/Test/User/User.js
@@ -2,23 +2,50 @@ import './user.scss';
 import { useEffect, useState } from 'react';
 import { Redirect } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
-import { supabase } from '../../../supabaseClient'
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
+import AlertMesg from '../../Utils/AlertMesg/AlertMesg';
+import { supabase } from '../../../supabaseClient';
 
 
 function Home() {
     const [redirect, setRedirect] = useState(null);
 
-    const { reset, register, handleSubmit, watch, setValue } = useForm({
+    // Validation du formulaire
+    const schema = yup.object().shape({
+        initials: yup.string()
+            .matches(/^[a-zA-Z-]+$/, {
+                excludeEmptyString: true,
+                message: "Ce champ n'accepte que les lettres et le tiret : A-Z"
+            })
+            .required("Merci de saisir vos initiales."),
+        birthDate: yup.date()
+            .typeError("Merci de vérifiez la date de naissance.")
+            .max(new Date(), "Vous n'êtes vraisemblablement pas un enfant du futur!")
+            .required("Merci de renseigner votre date de naissance."),
+        age: yup.number()
+            .typeError("Merci de renseigner votre âge.")
+            .integer("Merci de vérifiez l'âge.")
+            .moreThan(0, "Merci de vérifiez l'âge.")
+            .lessThan(99, "Merci de vérifiez l'âge.")
+            .required("Merci de saisir votre âge."),
+        gender: yup.string().oneOf(['M', 'F', 'O'], "Renseigner votre sexe.").required("Renseigner votre sexe."),
+        institution: yup.string().required("Merci de renseigner votre établissement",)
+    })
+
+    const { reset, register, handleSubmit, watch, setValue, formState: { errors } } = useForm({
         defaultValues: {
             'initials': '',
             'uniqueId': '',
             'birthDate': '',
-            'age': 0,
+            'age': '',
             'gender': 'M',
             'institution': ''
-        }
+        },
+        resolver: yupResolver(schema),
     })
 
+    // Calcul automatique de l'âge
     const watchBirthDate = watch('birthDate');
     useEffect(() => {
         const d = watchBirthDate.split('-');
@@ -26,7 +53,7 @@ function Home() {
         const d1 = new Date(); //aujourd'hui
         const d2 = new Date(d[0], d[1] - 1, d[2]); // Y,M,D (month counter is zero based)
         const age = d1.getFullYear() - d2.getFullYear();
-        if (!Number.isNaN(age) && age < 100) {
+        if (!Number.isNaN(age) && age > 0 && age < 100) {
             setValue('age', age)
         }
     }, [watchBirthDate, setValue]);
@@ -49,7 +76,7 @@ function Home() {
             const defaultValues = {
                 'uniqueId': JSON.parse(localStorage.getItem('user')).uniqueId,
                 'initials': JSON.parse(localStorage.getItem('user')).uniqueId.substring(0, 2),
-                'birthDate': JSON.parse(localStorage.getItem('user')).birthDate,
+                'birthDate': new Date(JSON.parse(localStorage.getItem('user')).birthDate).toISOString().substring(0,10), // récupérer la date sous la form 'yyyy-MM-dd'
                 'age': JSON.parse(localStorage.getItem('user')).age,
                 'gender': JSON.parse(localStorage.getItem('user')).gender,
                 'institution': JSON.parse(localStorage.getItem('user')).institution
@@ -60,8 +87,13 @@ function Home() {
 
     const onSubmit = async (vals) => {
         // Création de `uniqueId` : initiales + (année, mois, jour collés)
-        const d = vals.birthDate.split('-').join('');
-        const uniqueId = vals.initials.toUpperCase() + d;
+        console.log(vals)
+        // const d = vals.birthDate.split('-').join('');
+        const uniqueId = vals.initials.toUpperCase() 
+                            + vals.birthDate.getFullYear() 
+                            + (vals.birthDate.getMonth() + 1).toString().padStart(2, '0')
+                            + (vals.birthDate.getDate()).toString().padStart(2, '0');
+        console.log("id: ", uniqueId)
         const obj = {
             'uniqueId': uniqueId,
             'birthDate': vals.birthDate,
@@ -96,6 +128,9 @@ function Home() {
                                 {...register("initials")}
                                 type="text"
                                 className="form-control text-uppercase" />
+                            {errors.initials &&
+                                <AlertMesg message={errors.initials?.message} />
+                            }
                         </div>
                     </div>
                     <div className="row align-items-center mb-3">
@@ -105,6 +140,9 @@ function Home() {
                                 {...register("birthDate")}
                                 type="date"
                                 className="form-control" />
+                            {errors.birthDate &&
+                                <AlertMesg message={errors.birthDate?.message} />
+                            }
                         </div>
                     </div>
                     <div className="row align-items-center mb-3">
@@ -113,7 +151,11 @@ function Home() {
                             <input
                                 {...register("age")}
                                 type="number"
+                                step="0.01"
                                 className="form-control" />
+                            {errors.age &&
+                                <AlertMesg message={errors.age?.message} />
+                            }
                         </div>
                     </div>
                     <div className="row mb-3">
@@ -128,6 +170,9 @@ function Home() {
                             <label className="form-label d-block">
                                 <input type='radio' {...register("gender")} value='O' className="form-check-input" />
                                 Autre</label>
+                            {errors.gender &&
+                                <AlertMesg message={errors.gender?.message} />
+                            }
                         </div>
                     </div>
 
@@ -140,6 +185,9 @@ function Home() {
                                 name="institution"
                                 placeholder="Nom de l'établissement"
                                 className="form-control" />
+                            {errors.institution &&
+                                <AlertMesg message={errors.institution?.message} />
+                            }
                         </div>
                     </div>
 
