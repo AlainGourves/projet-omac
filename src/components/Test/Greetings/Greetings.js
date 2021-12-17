470ff6296c7bc9506a9f799ab0e988bf47fde3fe
Alain Gourvès <alain.gourves@gmail.com>
2021-12-17 11:03:13 +0100

    cosmétique

--- src/components/Test/Greetings/Greetings.js
+++ src/components/Test/Greetings/Greetings.js
@@ -12,7 +12,7 @@ function Greetings({ greetings }) {
         if (localStorage.getItem('user')) {
             localStorage.removeItem('user');
         }
-        setRedirect('/test');
+        setRedirect('/');
     }
     if (redirect) {
         return <Redirect to={redirect} />
