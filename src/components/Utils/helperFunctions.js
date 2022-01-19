26d0d407842a651ad997ac0f9b80bbd64400579b
Alain Gourvès <alain.gourves@gmail.com>
2022-01-19 12:44:02 +0100

    Add helper function

--- src/components/Utils/helperFunctions.js
+++ src/components/Utils/helperFunctions.js
@@ -28,3 +28,34 @@ export const addToLocalStorage = (key, val) => {
         console.warn("Erreur enregistrement localStorage", err);
     }
 }
+
+// Générer des mots de passe de 10 chars
+export const generatePassword = () => {
+    const specials = ['_', '-', '!', '*', '/', '&', '#', '+', '=', '%', '$'];
+    // position aléatoire [0-n]
+    const randomPos = (n) => {
+        return Math.floor(Math.random() * n);
+    }
+    // random special char
+    const randomSpecial = () => {
+        return specials[randomPos(specials.length)];
+    }
+    let str1 = Math.random().toString(36).slice(-6);
+    let str2 = Date.now().toString(36).substring(5, 8); // 3 chars
+    // place str2 à une position aléatoire dans str1
+    let pos = randomPos(str1.length);
+    // array de  9 caractères [a-z0-9]
+    let password = [str1.slice(0, pos), str2, str1.slice(pos)].join('').split('');
+    // Insère un caractère spécial à une position aléatoire (mais pas en premier)
+    password.splice(1 + randomPos(8), 0, randomSpecial());
+    // Entre 1 & 3 lettres en CAP
+    let n = 1 + randomPos(2);
+    while (n > 0) {
+        let l = randomPos(password.length);
+        if (!Number(password[l]) && password[l] !== 'i' && password[l] !== 'l') {
+            password[l] = password[l].toUpperCase();
+            n--;
+        }
+    }
+    return password.join('');
+}
